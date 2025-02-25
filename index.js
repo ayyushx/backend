d032c53a9abe4c07ca3900b891fc10d57fe6ca02
Ayush Badoni <ayushbadoni36@gmail.com>
2025-02-25 08:40:00 +0530

    .env processing learnt

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config() //To import the usage of .env file
+const express = require('express');
+const app = express();
+
+const port = 4000; //This is local variable but we also have global variable declared inside .env file
+
+app.get('/', (req,res) => {
+    res.send("Hello World");
+});
+
+app.get('/twitter', (req,res) => {
+    res.send("<h1>Twitter Page</h1>");
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`listening at port ${process.env.PORT}`);
+});
