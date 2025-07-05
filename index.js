d6367fa1607cf385437d7027714ba8cf7cc01423
Ayush Badoni <ayushbadoni36@gmail.com>
2025-07-05 08:05:35 +0530

    some most important learning

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config() //To import the usage of .env file
 const express = require('express');
 const app = express();
 
-const port = 4000 | process.env.PORT; //This is local variable but we also have global variable declared inside .env file
+const port = 4000 || process.env.PORT; //This is local variable but we also have global variable declared inside .env file
 
 app.get('/', (req,res) => {
     res.send("Hello World");
@@ -10,6 +10,10 @@ app.get('/', (req,res) => {
 
 app.get('/twitter', (req,res) => { // the '/' demonstrates the page we are going to visit
     res.send("<h1>Twitter Page</h1>");
+})
+
+app.get('/welcome', (req,res) =>{
+    res.send("<h1>Just got back to learn backend web development, that'y a new page</h1> ")
 })
 
 app.listen(port, () => {
